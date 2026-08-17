d62a768cf39a408f7a2cc425244d353685192bec
54145a <133004095+54145a@users.noreply.github.com>
2026-08-17 14:49:54 +0800

    add @jsquash/avif as devDep for types, remove hand-written d.ts

--- src/types/avif.d.ts
+++
@@ -1,4 +0,0 @@
-declare module "https://cdn.jsdelivr.net/npm/@jsquash/avif@2.1.1/+esm" {
-	export function init(options: { locateFile: (path: string) => string }): void;
-	export function encode(data: ImageData, options?: { quality?: number }): Promise<ArrayBuffer>;
-}
---
+++ src/types/cdn.d.ts
@@ -0,0 +1,3 @@
+declare module "https://cdn.jsdelivr.net/npm/@jsquash/avif@*/+esm" {
+	export { init, encode, decode } from "@jsquash/avif";
+}
